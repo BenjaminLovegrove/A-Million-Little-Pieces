b61e1b69b1c7de07d202db091aaf7e632a599d54
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-06-09 10:05:58 +1000

    Walking sfx. Enemies pull you in.

---
+++ A Million Little Pieces/Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public SpriteRenderer speechBubble;
+	
+	void OnTriggerEnter (Collider col) {
+		if ((col.transform.position.x < transform.position.x) && transform.localScale.x > 0) {
+			transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y,transform.localScale.z);
+			Invoke ("ResetScale", 15f);
+		}
+
+		if ((col.transform.position.x > transform.position.x) && transform.localScale.x < 0) {
+			transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y,transform.localScale.z);
+			Invoke ("ResetScale", 15f);
+		}
+
+		speechBubble.enabled = true;
+		col.SendMessage ("Spotted", transform.position, SendMessageOptions.DontRequireReceiver);
+		Invoke ("Reset", 4f);
+	}
+
+	void Reset (){
+		speechBubble.enabled = false;
+	}
+
+	void ResetScale(){
+		transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y,transform.localScale.z);
+	}
+	
+}
--- A Million Little Pieces/Assets/Scripts/Player.cs
+++ A Million Little Pieces/Assets/Scripts/Player.cs
@@ -5,21 +5,75 @@ public class Player : MonoBehaviour {
 
 	public float speedMod = 5f;
 	Animator anim;
+	AudioSource runningSFX;
+	bool moveLock = false;
+	Vector3 enemyTargetPos;
+	float lerpTimer = 2f;
 
 	void Start () {
 		anim = GetComponent<Animator> ();
+		runningSFX = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//Movement
-		transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * speedMod, Input.GetAxis ("Vertical") * Time.deltaTime * speedMod, 0);
+		if (!moveLock) {
+			transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * speedMod, Input.GetAxis ("Vertical") * Time.deltaTime * speedMod, 0);
 
-		//Animation
-		if ((Input.GetAxis ("Horizontal") != 0) || (Input.GetAxis ("Vertical") != 0)) {
-			anim.SetBool ("Walking", true);
-		} else {
-			anim.SetBool ("Walking", false);
+			//Animation
+			if ((Input.GetAxis ("Horizontal") != 0) || (Input.GetAxis ("Vertical") != 0)) {
+				anim.SetBool ("Walking", true);
+				if (!runningSFX.isPlaying) {
+					runningSFX.Play ();
+				}
+			} else {
+				anim.SetBool ("Walking", false);
+				if (runningSFX.isPlaying) {
+					runningSFX.Stop ();
+				}
+			}
 		}
+
+		if (lerpTimer < 1f) {
+			lerpTimer += 0.05f * Time.deltaTime;
+			//if (Vector2.Distance (transform.position, enemyTargetPos) > 3.5f) {
+			if (((enemyTargetPos.x - transform.position.x) > 3.5f) || ((enemyTargetPos.y - transform.position.y) > 1f)){
+				transform.position = Vector3.Lerp (transform.position, enemyTargetPos, lerpTimer);
+				anim.SetBool ("Walking", true);
+				if (!runningSFX.isPlaying) {
+					runningSFX.Play ();
+				}
+			} else {
+				anim.SetBool ("Walking", false);
+				if (runningSFX.isPlaying) {
+					runningSFX.Stop ();
+				}
+			}
+		}
+	}
+
+	void Spotted(Vector3 enemypos){
+		if (!moveLock) {
+			LockMovement ();
+			enemyTargetPos = enemypos;
+			Invoke ("MoveTo", 1f);
+		}
+	}
+
+	void MoveTo(){
+		lerpTimer = 0f;
+	}
+
+	void LockMovement(){
+		moveLock = true;
+		anim.SetBool ("Walking", false);
+		if (runningSFX.isPlaying) {
+			runningSFX.Stop ();
+		}
+	}
+
+	void UnlockMovement(){
+		moveLock = false;
 	}
 }
